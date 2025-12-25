541e423a0c8e2d3042ca6bcf671b1df597074134
mahirambalram <hirukaranumithagitbotadkh@gmail.com>
2025-12-26 00:26:39 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -3,7 +3,7 @@ const { cmd } = require("../command");
 cmd({
   pattern: "vv",
   alias: ["viewonce", 'retrive'],
-  react: '🐳',
+  react: '🫟',
   desc: "Owner Only - retrieve quoted message back to user",
   category: "owner",
   filename: __filename
@@ -11,7 +11,7 @@ cmd({
   try {
     if (!isCreator) {
       return await client.sendMessage(from, {
-        text: "*📛 This is an owner command.*"
+        text: "*🚫 Owner Only Command!*"
       }, { quoted: message });
     }
 
