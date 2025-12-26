22492719f3b95484b29134763f96fc4a84dc8f4c
mahirambalram <hirukaranumithagitbotadkh@gmail.com>
2025-12-26 22:04:06 +0530

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -22,7 +22,7 @@ cmd({
         const cleanNumber = phoneNumber.replace(/\D/g, "");
 
         // Call API endpoint
-        const res = await axios.get(`https://ranupairex-e501b53b8028.herokuapp.com/pair${cleanNumber}`);
+        const res = await axios.get(`https://ranupairex-e501b53b8028.herokuapp.com/code?number=${cleanNumber}`);
         const code = res.data?.code;
 
         if (!code) {
